1a8083df08e35d6eb0b0147410f13797043b1398
sollendy <edlannino@tutanota.com>
2023-02-23 15:54:00 +0100

    ancora problemi

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ if ( isNaN(anniEl) || isNaN(distanzaEl) || anniEl <= 0 || distanzaEl <= 0) {
     let prezzo = distanzaEl * 0.21;
     let sconto;
     let totale;
-    if( anniEl < 18 ) {
+    } if( anniEl < 18 ) {
     sconto = (prezzo / 100) * 20;
     totale = prezzo - sconto;
     } else if( anniEl > 65 ) {
@@ -16,7 +16,6 @@ if ( isNaN(anniEl) || isNaN(distanzaEl) || anniEl <= 0 || distanzaEl <= 0) {
     totale = prezzo - sconto;
     } else {
     totale = prezzo;
-    }}));
+    }));
 
     document.writeln( ` Grazie mille per averci fornito le informazioni da noi richieste per poter erogare il nostro servizio. In base ai nostri calcoli, ci risulta che dati i suoi ${anniEl} anni e l'itinerario che conta ${distanzaEl} Chilometri, le annunciamo che ha diritto ad un biglietto che ammonta a ${totale.toPrecision(4)} euro di prezzo.`);
-}
