7fd2f6186f7c38e4576c0d6cc14fa272d91d076b
sollendy <edlannino@tutanota.com>
2023-02-23 16:56:10 +0100

    problemi con resa e console.log

--- js/script.js
+++ js/script.js
@@ -2,20 +2,26 @@ let anniEl = document.getElementById("age");
 let distanzaEl = document.getElementById("distanza");
 let bottoneEl = document.getElementById("insertBtn");
 
-if ( isNaN(anniEl) || isNaN(distanzaEl) || anniEl <= 0 || distanzaEl <= 0) {
-    alert("errore nella digitazione dei caratteri");
-    } else if (bottoneEl.addEventListener('click', function() {
-    let prezzo = distanzaEl * 0.21;
-    let sconto;
-    let totale;
-    } if( anniEl < 18 ) {
-    sconto = (prezzo / 100) * 20;
-    totale = prezzo - sconto;
-    } else if( anniEl > 65 ) {
-    sconto = (prezzo / 100) * 40;
-    totale = prezzo - sconto;
-    } else {
-    totale = prezzo;
-    }));
 
-    document.writeln( ` Grazie mille per averci fornito le informazioni da noi richieste per poter erogare il nostro servizio. In base ai nostri calcoli, ci risulta che dati i suoi ${anniEl} anni e l'itinerario che conta ${distanzaEl} Chilometri, le annunciamo che ha diritto ad un biglietto che ammonta a ${totale.toPrecision(4)} euro di prezzo.`);
+
+    bottoneEl.addEventListener('click', function() {
+        if ( isNaN(anniEl.value) || isNaN(distanzaEl.value) || anniEl.value <= 0 || distanzaEl.value <= 0) {
+            alert("errore nella digitazione dei caratteri");
+        }
+
+            let prezzo = distanzaEl.value * 0.21;
+            let sconto;
+            let totale;
+
+        if ( anniEl.value < 18 ) {
+             sconto = (prezzo / 100) * 20;
+             totale = prezzo - sconto;
+            } else if( anniEl.value > 65 ) {
+             sconto = (prezzo / 100) * 40;
+             totale = prezzo - sconto;
+            } else {
+             totale = prezzo;
+            }
+
+            console.log( "totale: €"totale.toPrecision(4)} );
+    })  
