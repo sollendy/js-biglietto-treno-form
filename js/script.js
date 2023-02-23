2c3278885a4555c4875265b45efc713e94358fd5
sollendy <edlannino@tutanota.com>
2023-02-23 15:38:49 +0100

    problemi con realizzazione

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-let anni = document.getElementById("age");
-let distanza = document.getElementById("distanza");
-if ( isNaN(anni) || isNaN(distanza) || anni <= 0 || distanza <= 0) {
-    //alert("errore nella digitazione dei caratteri");
-    } else {
-    let prezzo = distanza * 0.21;
+let anniEl = document.getElementById("age");
+let distanzaEl = document.getElementById("distanza");
+let bottoneEl = document.getElementById("insertBtn");
+
+if ( isNaN(anniEl) || isNaN(distanzaEl) || anniEl <= 0 || distanzaEl <= 0) {
+    alert("errore nella digitazione dei caratteri");
+    } else if (bottoneEl.addEventListener('click', function() {
+    let prezzo = distanzaEl * 0.21;
     let sconto;
     let totale;
-    if( anni < 18 ) {
+    if( anniEl < 18 ) {
     sconto = (prezzo / 100) * 20;
     totale = prezzo - sconto;
-    } else if( anni > 65 ) {
+    } else if( anniEl > 65 ) {
     sconto = (prezzo / 100) * 40;
     totale = prezzo - sconto;
     } else {
     totale = prezzo;
-    }
+    }}));
 
-    document.writeln( ` Grazie mille per averci fornito le informazioni da noi richieste per poter erogare il nostro servizio. In base ai nostri calcoli, ci risulta che dati i suoi ${anni} anni e l'itinerario che conta ${distanza} Chilometri, le annunciamo che ha diritto ad un biglietto che ammonta a ${totale.toPrecision(4)} euro di prezzo.`);
+    document.writeln( ` Grazie mille per averci fornito le informazioni da noi richieste per poter erogare il nostro servizio. In base ai nostri calcoli, ci risulta che dati i suoi ${anniEl} anni e l'itinerario che conta ${distanzaEl} Chilometri, le annunciamo che ha diritto ad un biglietto che ammonta a ${totale.toPrecision(4)} euro di prezzo.`);
 }
